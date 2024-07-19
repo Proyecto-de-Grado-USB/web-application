fed2a5004ae2e4702cb0ba65ae8485d79f7c3caf
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2024-07-19 12:56:47 -0400

    Add loans sql

--- repo/apps/web/script.sql
+++ repo/apps/web/script.sql
@@ -8,6 +8,17 @@ CREATE TABLE IF NOT EXISTS admin_users (
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS my_database.loans (
+    document_id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    expiration_date VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL
+);
+
 INSERT INTO admin_users (name, email, password) VALUES
 ('Pablo Terceros', 'user1@example.com', 'password1'),
 ('Andres Camacho', 'user2@example.com', 'password2');
+
+INSERT INTO my_database.loans (document_id, user_id, expiration_date, state) VALUES
+('978-0-8231-7820-9', '1', '20/07/2024', 'pending'),
+('978-0-19-715402-1', '2', '20/07/2024', 'completed');
