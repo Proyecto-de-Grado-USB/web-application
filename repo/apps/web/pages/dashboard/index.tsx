84393d6c544ae8825dee843da6dc04bda100eae7
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2024-07-25 23:46:51 -0400

    Base cards placement

--- repo/apps/web/pages/dashboard/index.tsx
+++ repo/apps/web/pages/dashboard/index.tsx
@@ -7,7 +7,6 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Chart from "./Chart";
 import Card from "./MetricsCard";
-import Orders from "./Orders";
 import { Helmet } from "react-helmet";
 import AppBarWithDrawer from "@/components/AppBarWithDrawer";
 import Typography from "@mui/material/Typography";
@@ -42,18 +41,26 @@ export default function Dashboard() {
       <ThemeProvider theme={defaultTheme}>
         <Box sx={{ display: "flex" }}>
           <CssBaseline />
-          <AppBarWithDrawer title={"Panel Administrativo"}/>
-          <Box component="main" sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}>
-            <Container maxWidth="lg" sx={{ my: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <AppBarWithDrawer title={"Panel Administrativo"} />
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Container
+              maxWidth="lg"
+              sx={{ my: 4, display: "flex", flexDirection: "column", alignItems: "center" }}
+            >
               <Grid container spacing={3}>
                 <Grid item xs={12} md={8} lg={9}>
                   <Paper sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}>
@@ -62,12 +69,27 @@ export default function Dashboard() {
                 </Grid>
                 <Grid item xs={12} md={4} lg={3}>
                   <Paper sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}>
-                    <Card title="Documentos Añadidos" activityType="modify"/>
+                    <Card title="Documentos Añadidos" activityType="insert" />
                   </Paper>
                 </Grid>
-                <Grid item xs={12}>
-                  <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                    <Orders />
+                <Grid item xs={12} md={6} lg={6}>
+                  <Paper sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}>
+                    <Card title="Documentos Eliminados" activityType="delete" />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} md={6} lg={6}>
+                  <Paper sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}>
+                    <Card title="Documentos Actualizados" activityType="modify" />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} md={6} lg={6}>
+                  <Paper sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}>
+                    <Card title="Estado de los Préstamos" activityType="review" />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} md={6} lg={6}>
+                  <Paper sx={{ p: 2, display: "flex", flexDirection: "column", height: 240 }}>
+                    <Card title="Préstamos Únicos y Totales" activityType="modify_today" />
                   </Paper>
                 </Grid>
               </Grid>
