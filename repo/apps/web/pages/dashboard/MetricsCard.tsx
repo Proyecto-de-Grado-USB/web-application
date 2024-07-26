91d0b0c9404361e00f1cb0e99f28d461dd54a626
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2024-07-25 22:53:12 -0400

    Implement activity by day

--- repo/apps/web/pages/dashboard/MetricsCard.tsx
+++ repo/apps/web/pages/dashboard/MetricsCard.tsx
@@ -4,15 +4,38 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
+import moment from 'moment-timezone';
 
 export default function Card() {
   const [view, setView] = React.useState('day');
+  const [count, setCount] = React.useState(0);
 
   const handleViewChange = (event: React.MouseEvent<HTMLElement>, newView: string) => {
     if (newView !== null) {
       setView(newView);
     }
   };
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:3001/api/activities');
+        const activities = await response.json();
+
+        const date = moment().tz('America/La_Paz').format('YYYY-MM-DD');
+        const filteredActivities = activities.filter((activity: { action_type: string, action_date: string }) => 
+          activity.action_type === 'modify' && activity.action_date.startsWith(date)
+        );
+
+        setCount(filteredActivities.length);
+
+      } catch (error) {
+        console.error('Error fetching activities:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <React.Fragment>
@@ -28,7 +51,7 @@ export default function Card() {
       >
         <Title>Documentos Añadidos</Title>
         <Typography component="p" variant="h2" marginTop={0.5}>
-          17
+          {count}
         </Typography>
         <Typography color="text.secondary" sx={{ flex: 1 }}>
           en Total
